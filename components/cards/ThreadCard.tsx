270a70eaaa3688a8f75e62d333ef49eecc771424
Giovanna <giovannaduarte281999@gmail.com>
2023-10-08 13:04:32 -0300

    final commit

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,4 +1,4 @@
-import { Libre_Bodoni } from "next/font/google";
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -102,7 +102,7 @@ const ThreadCard = ({
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
                   <p className="mt-1 text-subtle-medium text-gray-1">
-                    {comments.length}
+                    {comments.length} replies
                   </p>
                 </Link>
               )}
@@ -110,6 +110,24 @@ const ThreadCard = ({
           </div>
         </div>
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} - {community.name} Community
+          </p>
+
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
